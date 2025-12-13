2619fc702a65ff71b8d2ba8ba71e3a2e7937761f
davirezo <fksdavi@gmail.com>
2025-12-13 13:39:37 -0300

    DepartmentDao implentacao and Test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -3,9 +3,14 @@ package model.dao;
 import db.DB;
 import modelDaoImpl.SellerDaoJDBC;
 
+
 public class DaoFactory {
 
-    public static SellerDao createSellerDao(){
+    public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new modelDaoImpl.DepartmentDaoJDBC(DB.getConnection());
     }
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -4,12 +4,11 @@ import model.entities.Department;
 
 import java.util.List;
 
-public interface DepartamentDao {
+public interface DepartmentDao {
 
     void insert(Department obj);
-    void update (Department obj);
-    void deletedById(Integer Id);
+    void update(Department obj);
+    void deleteById(Integer id);
     Department findById(Integer id);
     List<Department> findAll();
-
 }
